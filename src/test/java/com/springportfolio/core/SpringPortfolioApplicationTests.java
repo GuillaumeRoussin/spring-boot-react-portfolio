8c67f47bb5f4dc481d0285e2f50bd964bee937b8
GuillaumeRoussin <guillaumeroussin.pr@gmail.com>
2024-08-30 12:25:48 +0200

    Initialize

---
+++ src/test/java/com/springportfolio/core/SpringPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springportfolio.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
